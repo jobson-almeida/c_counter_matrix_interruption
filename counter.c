abcb4681ee344ae0fefcbc19088c6affffed7edc
Jobson Almeida <jobson.ra@outlook.com>
2025-01-30 16:51:31 -0300

    refactor: adicionar comentários descritivos no cabeçalho da biblioteca

--- counter.c
+++ counter.c
@@ -1,11 +1,20 @@
+#include "counter.h"
 #include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 
 #define NUM_PIXELS 25 // quantidade de LEds da matriz
 
-// frames da animação no formato hexadecimal que representam cada um dos números de 0 a 9, específicos para a matriz 5x5 da placa de desenvolvimento BitLogLab
-//                                           0          1          2          3          4          5          6          7          8          9
+/* 
+* frames/itens estão no formato hexadecimal, que representam cada um dos números de 0 a 9, uma representação específica para a matriz 5x5 da placa de desenvolvimento BitLogLab
+* O valor 0x1FCCF3F, por exemplo, se refere a 1111111001100111100111111 que corresponde a exibição do número '0' na matriz da placa educacional BitDogLab
+* O valor também é representado pela matriz:
+*  11111
+*  11001
+*  10011
+*  11001
+*  11111
+*                                            0          1          2          3          4          5          6          7          8          9      */
 uint32_t counter_animation_frames[10] = {0x1FCCF3F, 0x03C0F03, 0x1FC7C7F, 0x1FC3F1F, 0x1BDFF03, 0x1F1FF1F, 0x1F1FF7F, 0x1FC0F03, 0x1FCFF3F, 0x1FCFF1F};
 
  double *apply_intensity_frame(uint32_t frame, double intensity)
--- counter.h
+++ counter.h
@@ -3,16 +3,43 @@
  
 #include "hardware/pio.h"
 
-// função para configurar a intensidade de cada LED em cada frame
+/**
+ * @brief Função que permite configurar a intensidade dos leds de um frame
+ * @param frame Item da animação que se deseja aplicar a intensidade 
+ * @param intensity Valor atribuido a intensidade de brilho do LED de 0.0 à 1.0
+ */
 double *apply_intensity_frame(uint32_t frame, double intensity);
 
-// função para converter a cor RGB em binário
+/**
+ * @brief Função que permite a conversão da cor, no format RGB, em um valor binário
+ * @param r Valor referente a cor vermelha
+ * @param g Valor referente a cor verde
+ * @param b Valor referente a cor azul
+ */
 uint32_t rgb(uint8_t r, uint8_t g, uint8_t b);
 
-// função para aplicar cor aos leds conforme parâmetros definidos pelo usuário e a intensidade predefinida no frame
-void apply_color_frame(double *desenho, PIO pio, uint sm, uint8_t r, uint8_t g, uint8_t b);
+/**
+ * @brief Função que permite aplicar uma determinada cor, no padrão RGB, ao led
+ * @note O efeito luminoso produzido pela cor será influencido pela intensidade predefinida nos framee
+ * @param number Valor referente ao frame do número que se deseja aplicar a cor
+ * @param pio Valor referente a interface PIO
+ * @param sm Valor referente a máquina de estados
+ * @param r Valor referente a cor vermelha
+ * @param g Valor referente a cor verde
+ * @param b Valor referente a cor azul
+ */
+void apply_color_frame(double *number, PIO pio, uint sm, uint8_t r, uint8_t g, uint8_t b);
 
-// exibir frame / representação de um número
+/**
+ * @brief Função responsável por exibir um determinado número na matriz de LEDs
+ * @note O efeito produzido pela cor será influencido pela intensidade predefinida no frame
+ * @param pio Valor referente a interface PIO
+ * @param sm Valor referente a máquina de estados
+ * @param r Valor referente a cor vermelha
+ * @param g Valor referente a cor verde
+ * @param b Valor referente a cor azul
+ * @param frame Valor referente ao número (0-9) que se deseja visualizar na matriz
+ */
 void show_number(PIO pio, uint sm, uint8_t r, uint8_t g, uint8_t b, double intensity, size_t frame);
 
 #endif
