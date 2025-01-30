90c7180b8c02ee1009b0d0e8cd45014115b56b16
Jobson Almeida <jobson.ra@outlook.com>
2025-01-30 13:34:04 -0300

    chore: criar os frames dos números de 0 a 9 a serem exibidos na matriz de LEDs

---
+++ counter.c
@@ -0,0 +1,3 @@
+// frames da animação no formato hexadecimal que representam cada um dos números de 0 a 9, específicos para a matriz 5x5 da placa de desenvolvimento BitLogLab
+                                     //      0          1          2          3          4          5          6          7          8          9
+uint32_t counter_animation_frames[10] = {0x1FCCF3F, 0x03C0F03, 0x1FC7C7F, 0x1FC3F1F, 0x1BDFF03, 0x1F1FF1F, 0x1F1FF7F, 0x1FC0F03, 0x1FCFF3F, 0x1FCFF1F};
