2fb591e63330f636dd63083fab8db10dbd15815e
Jobson Almeida <jobson.ra@outlook.com>
2025-01-30 15:26:44 -0300

    build: declarar função de exibição dos números no cabeçalho da biblioteca

--- counter.h
+++ counter.h
@@ -4,5 +4,7 @@
 // função para configurar a intensidade de cada LED em cada frame
 double *apply_intensity_frame(uint32_t frame, double intensity);
 
+// exibir frame / representação de um número
+void show_number(PIO pio, uint sm, uint8_t r, uint8_t g, uint8_t b, double intensity, size_t frame);
 
 #endif
